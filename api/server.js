d8541115ae494032c3e8279785e3a8591e2e7e3b
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-08 16:49:57 -0500

    Starting Cookie

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router')
 const authRouter = require('./auth/auth-router')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -17,6 +19,25 @@ const authRouter = require('./auth/auth-router')
  */
 
 const server = express();
+
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'keep it secret',
+  cookie: {
+    maxAge: 1000 * 60 * 5,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false, 
+  saveUninitialized: false, 
+  store: new Store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
 
 server.use(helmet());
 server.use(express.json());
